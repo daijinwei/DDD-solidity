d4ac3166b77212a2da6d408ee36dc882a9dc34f2
thinkmyself41 <daijinwei41@163.com>
2024-07-04 22:19:36 +0800

    Add default values

---
+++ 09_default_vars/default_vars.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract DefaultValues{
+    uint public count;
+    bool public _bool;
+    int public _int;
+    address public _address;
+    bytes32  public _bypes;
+}
