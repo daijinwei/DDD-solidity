4496558e2f07fb5b060882d175ab8e53eb0c4fad
thinkmyself41 <daijinwei41@163.com>
2024-07-11 12:30:09 +0800

    Move base func to base dir

--- base/15_constructor/constructor.sol
+++ base/15_constructor/constructor.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.0;
 
 contract ConstructorExcample {
-    address public addr;
+    address public owner;
     uint public storageData;
     // 构造函数，和c++构造函数用途一样，只在部署合约时时用，初始化一些合约状态变量。
     constructor(uint _x) {
-        addr = msg.sender;
+        owner = msg.sender;
         storageData = _x;
     }
     function getData()external view returns(uint){
